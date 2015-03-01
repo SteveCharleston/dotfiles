dee8e664d9adb3324346fbd318598880f743fe50
Steve Charleston <schalhorn@gmail.com>
2015-03-01 20:34:44 +0100

    Add install.sh file for symlinking of dotfiles.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+function link_if_missing() {
+    SRC=$1
+    DEST=$2
+
+    if ! [ -L $DEST ]; then
+        ln -ivs $SRC $DEST
+    else
+        echo "Skipping, link already exists: $DEST"
+    fi
+}
+
+link_if_missing $PWD/.vimrc $HOME/.vimrc
+link_if_missing $PWD/.vim $HOME/.vim
