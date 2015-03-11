f7f84b3019e001e2fd69a906be3184e14bd332fa
U-INFO\steven.schalhorn <steven.schalhorn@client7-34.INFO.TIME-FOR-KIDS>
2015-03-11 14:02:28 +0100

    Added .minttyrc

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ link_if_missing $PWD/.vimrc $HOME/.vimrc
 link_if_missing $PWD/.vim $HOME/.vim
 link_if_missing $PWD/.bashrc $HOME/.bashrc
 link_if_missing $PWD/.tmux.conf $HOME/.tmux.conf
+link_if_missing $PWD/.minttyrc $HOME/.minttyrc
 
 link_if_missing $PWD/bin/tree $HOME/bin/tree
 chmod 0755 $HOME/bin/tree
