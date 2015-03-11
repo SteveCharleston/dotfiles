2e7d54e1525b7dfab443cc3afffafb2aa15094b5
U-INFO\steven.schalhorn <steven.schalhorn@client7-34.INFO.TIME-FOR-KIDS>
2015-03-11 12:19:16 +0100

    Added making tree executable to install script

--- install.sh
+++ install.sh
@@ -11,8 +11,13 @@ link_if_missing() {
     fi
 }
 
+mkdir -p $HOME/bin
+chmod 0755 $HOME/bin
+
 link_if_missing $PWD/.vimrc $HOME/.vimrc
 link_if_missing $PWD/.vim $HOME/.vim
 link_if_missing $PWD/.bashrc $HOME/.bashrc
 link_if_missing $PWD/.tmux.conf $HOME/.tmux.conf
+
 link_if_missing $PWD/bin/tree $HOME/bin/tree
+chmod 0755 $HOME/bin/tree
