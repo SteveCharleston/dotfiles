8224e32142e3aa6b8ae2776a7353fb2c944b7535
U-INFO\steven.schalhorn <steven.schalhorn@client7-34.INFO.TIME-FOR-KIDS>
2015-03-16 09:15:17 +0100

    Using mytree instead of foreign tree script with linking now

--- install.sh
+++ install.sh
@@ -20,5 +20,5 @@ link_if_missing $PWD/.bashrc $HOME/.bashrc
 link_if_missing $PWD/.tmux.conf $HOME/.tmux.conf
 link_if_missing $PWD/.minttyrc $HOME/.minttyrc
 
-link_if_missing $PWD/bin/tree $HOME/bin/tree
+link_if_missing $PWD/bin/mytree.py $HOME/bin/tree
 chmod 0755 $HOME/bin/tree
