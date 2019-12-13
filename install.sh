7b05f991274a4464e074e47835e2a301cabc76ab
SteveCharleston <schalhorn@gmail.com>
2019-12-13 22:48:52 +0100

    Linking of coc-settings.json file

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ link_if_missing $PWD/.bashrc $HOME/.bashrc
 link_if_missing $PWD/.tmux.conf $HOME/.tmux.conf
 link_if_missing $PWD/.minttyrc $HOME/.minttyrc
 link_if_missing $PWD/.ideavimrc $HOME/.ideavimrc
+link_if_missing $PWD/coc-settings.json $HOME/.config/nvim/coc-settings.json
 
 link_if_missing $PWD/bin/mytree.py $HOME/bin/tree
 chmod 0755 $HOME/bin/tree
