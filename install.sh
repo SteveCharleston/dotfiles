263986fbf28440d52023edf51c8fd862177f99d7
SteveCharleston <schalhorn@gmail.com>
2019-12-29 13:16:24 +0100

    Use same coc-settings.json in vim and nvim

--- install.sh
+++ install.sh
@@ -22,6 +22,7 @@ link_if_missing $PWD/.tmux.conf $HOME/.tmux.conf
 link_if_missing $PWD/.minttyrc $HOME/.minttyrc
 link_if_missing $PWD/.ideavimrc $HOME/.ideavimrc
 link_if_missing $PWD/.coc-settings.json $HOME/.config/nvim/coc-settings.json
+link_if_missing $PWD/.coc-settings.json $HOME/.vim/coc-settings.json
 
 link_if_missing $PWD/bin/mytree.py $HOME/bin/tree
 chmod 0755 $HOME/bin/tree
