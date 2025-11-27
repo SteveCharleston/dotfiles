6b02f579799a184e2ea7dc6044a5f0bfb5c95af4
SteveCharleston <steven@schalhorn.org>
2025-11-27 18:30:35 +0100

    nvim: Close NERDTree if it's the last window
    Signed-off-by: SteveCharleston <steven@schalhorn.org>

--- nvim/lua/plugins/filebrowse.lua
+++ nvim/lua/plugins/filebrowse.lua
@@ -12,6 +12,10 @@ return {
             vim.g.NERDTreeCascadeSingleChildDir = 0
             vim.g.NERDTreeDirArrowExpandable = '▸'
             vim.g.NERDTreeDirArrowCollapsible = '▾'
+            vim.cmd([[
+                " Exit Vim if NERDTree is the only window remaining in the only tab.
+                autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | call feedkeys(":quit\<CR>:\<BS>") | endif
+            ]])
         end,
     },
     {
