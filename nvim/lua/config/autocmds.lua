e90ee00c9a624ab2aac7432fe4b9e2cd13861850
SteveCharleston <steven@schalhorn.org>
2025-12-21 12:34:21 +0100

    nvim: Store yanked text into numbered registers
    Use an autocommand to store yanked text into numbered registers (1-9),
    which usually are used for storing deleted text. This way, yanked text
    is also available in these registers for later use.
    
    Signed-off-by: SteveCharleston <steven@schalhorn.org>

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -18,3 +18,14 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank({ higroup = "Visual", timeout = 400 })
     end,
 })
+
+-- Yank-ring: store yanked text in registers 1-9.
+vim.api.nvim_create_autocmd('TextYankPost', {
+    callback = function()
+    if vim.v.event.operator == 'y' then
+        for i = 9, 1, -1 do -- Shift all numbered registers.
+        vim.fn.setreg(tostring(i), vim.fn.getreg(tostring(i - 1)))
+        end
+    end
+    end,
+})
